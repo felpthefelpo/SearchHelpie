dca5aa8b19bee4d7cd90cdd357c2d68e83294fd4
felpthefelpo <felpoandrade7@gmail.com>
2023-04-15 19:05:37 -0300

    Adicionado borda aos botões do menu
    Reciclagem do código geral

--- felpiagl.py
+++ felpiagl.py
@@ -17,7 +17,7 @@ def search_google(query):
 
 
 def obter_resposta():
-    """Obtém a resposta da pesquisa do Google e exibe-a no painel de resposta"""
+    """Obtém a resposta da pesquisa do Google e exibe o link com a fonte da pesquisa"""
     pergunta = campo_pergunta.get()
     resposta = search_google(pergunta)
     rotulo_link.config(text=link_resposta) #atualiza o rótulo com o link
@@ -33,11 +33,6 @@ def obter_resposta():
         botao_busca = tk.Button(janela, text=resposta, command=lambda: webbrowser.open(link_resposta))
         botao_busca.pack(side=tk.TOP)
         botoes_buscas.append(botao_busca)
-
-def abrir_link():
-    """Abre o link no navegador padrão do sistema"""
-    resposta = rotulo_link.cget("text")
-    webbrowser.open(resposta)
 
 def reset():
     """Limpa o conteúdo do campo de pergunta e do campo de resposta"""
@@ -60,18 +55,16 @@ janela = tk.Tk()
 janela.title("SearchHelpie :)")
 
 # Cria os widgets da interface gráfica
-rotulo_link = tk.Label(janela, text="")
-botao_abrir_link = tk.Button(janela, text="Abrir link", command=abrir_link)
-botao_abrir_link.pack(side=tk.TOP)
-rotulo_link.pack(side=tk.TOP)
-botao_reset = tk.Button(janela, text="Reset", command=reset)
-botao_reset.pack(side=tk.TOP)
 
+rotulo_link = tk.Label(janela, text="")
+rotulo_link.pack(side=tk.TOP)
 rotulo_pergunta = tk.Label(janela, text="Em que posso te ajudar?")
 rotulo_pergunta.pack(side=tk.TOP)
 campo_pergunta = tk.Entry(janela, width=50)
 campo_pergunta.pack(side=tk.TOP)
-botao_pesquisar = tk.Button(janela, text="Pesquisar", command=obter_resposta)
+botao_reset = tk.Button(janela, text="Reset", command=reset, bd=4)
+botao_reset.pack(side=tk.RIGHT)
+botao_pesquisar = tk.Button(janela, text="Pesquisar", command=obter_resposta, bd=4)
 botao_pesquisar.pack(side=tk.TOP)
 rotulo_ultimas_buscas = tk.Label(janela, text="Últimas buscas:")
 rotulo_ultimas_buscas.pack(side=tk.TOP)
