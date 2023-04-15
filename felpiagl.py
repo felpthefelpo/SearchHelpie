264f9d993479cf5498d001c606f8944399bc120b
Felpthefelpo <felpoandrade7@gmail.com>
2023-04-15 03:54:50 -0300

    - Efetuado criação de botão de reset
    - pequenos ajustes no código para facilitar o desenvolvimento futuro

--- felpiagl.py
+++ felpiagl.py
@@ -4,27 +4,35 @@ import webbrowser
 
 def search_google(query):
     """Pesquisa no Google a resposta para a pergunta"""
+    global link_resposta
     try:
-        resposta = next(search(query, num_results=1))
+        link_resposta = next(search(query, num_results=1))
     except StopIteration:
-        resposta = "Não foi possível encontrar uma resposta para a pergunta."
-    return resposta
+        link_resposta = "Não foi possível encontrar uma resposta para a pergunta."
+    return link_resposta
 
 
 def obter_resposta():
     """Obtém a resposta da pesquisa do Google e exibe-a no painel de resposta"""
     pergunta = campo_pergunta.get()
     resposta = search_google(pergunta)
-    rotulo_link.config(text=resposta) #atualiza o rótulo com o link
+    rotulo_link.config(text=link_resposta) #atualiza o rótulo com o link
     campo_resposta.config(state='normal')
     campo_resposta.delete('1.0', tk.END)
-    campo_resposta.insert(tk.END, "Clique no link acima para abrir no navegador.")
+    campo_resposta.insert(tk.END, resposta)
     campo_resposta.config(state='disabled')
 
 def abrir_link():
     """Abre o link no navegador padrão do sistema"""
     resposta = rotulo_link.cget("text")
     webbrowser.open(resposta)
+
+def reset():
+    """Limpa o conteúdo do campo de pergunta e do campo de resposta"""
+    campo_pergunta.delete(0, tk.END)
+    campo_resposta.config(state='normal')
+    campo_resposta.delete('1.0', tk.END)
+    campo_resposta.config(state='disabled')
 
 # Cria a janela principal
 janela = tk.Tk()
@@ -35,14 +43,16 @@ rotulo_link = tk.Label(janela, text="")
 botao_abrir_link = tk.Button(janela, text="Abrir link", command=abrir_link)
 botao_abrir_link.pack(side=tk.TOP)
 rotulo_link.pack(side=tk.TOP)
+botao_reset = tk.Button(janela, text="Reset", command=reset)
+botao_reset.pack(side=tk.TOP)
 
-rotulo_pergunta = tk.Label(janela, text="Pergunta:")
+rotulo_pergunta = tk.Label(janela, text="Em que posso te ajudar?")
 rotulo_pergunta.pack(side=tk.TOP)
 campo_pergunta = tk.Entry(janela, width=50)
 campo_pergunta.pack(side=tk.TOP)
 botao_pesquisar = tk.Button(janela, text="Pesquisar", command=obter_resposta)
 botao_pesquisar.pack(side=tk.TOP)
-rotulo_resposta = tk.Label(janela, text="Resposta:")
+rotulo_resposta = tk.Label(janela, text="Resultado da busca:")
 rotulo_resposta.pack(side=tk.TOP)
 campo_resposta = tk.Text(janela, width=50, height=10, state='disabled')
 campo_resposta.pack(side=tk.TOP)
